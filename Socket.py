3f2ddfc6e5fce94d3406088e1ebb733b70d61ed6
PVkolos <70021890+PVkolos@users.noreply.github.com>
2022-01-01 12:41:33 +0300

    Update Socket.py

--- Socket.py
+++ Socket.py
@@ -1,5 +1,8 @@
+import json
 import socket
 import asyncio
+import struct
+from ex import SocketException
 
 
 class Socket:
@@ -12,11 +15,41 @@ class Socket:
         self.main_loop = asyncio.new_event_loop()
 
     async def send_data(self, **kwargs):
-        where = kwargs['where']
-        data = kwargs['data']
-        del kwargs['where']
-        del kwargs['data']
-        await self.main_loop.sock_sendall(where, data)
+        try:
+            where = kwargs['where']
+            del kwargs['where']
+            data = self._encode_data(kwargs)
+            meta_data = struct.pack(">I", len(data))
+            await self.main_loop.sock_sendall(where, meta_data + data)
+
+        except (KeyError, UnicodeEncodeError, ConnectionError, ValueError) as exc:
+            raise SocketException(exc)
+
+    async def _recv_message(self, listened_socket: socket.socket, message_len: int):
+        message = bytearray()
+
+        while len(message) < message_len:
+            packet = await self.main_loop.sock_recv(listened_socket, message_len - len(message))
+            if packet is None:
+                return None
+
+            message.extend(packet)
+
+        return message
+
+    def _encode_data(self, data):
+        return json.dumps(data).encode('utf-8')
+
+    def _decode_data(self, data: bytes):
+        return json.loads(data.decode('utf-8'))
 
     async def listen_socket(self, listened_socket):
-        pass
+        try:
+            meta_data = await self._recv_message(listened_socket, 4)
+            meta_data = struct.unpack(">I", meta_data)[0]
+
+            data = await self._recv_message(listened_socket, meta_data)
+            return self._decode_data(data)
+        except (UnicodeDecodeError, json.JSONDecodeError, IndexError, ConnectionError) as exc:
+            raise SocketException(exc)
+
